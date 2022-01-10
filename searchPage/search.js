feb8e022658e32268ec25801389d4520e89be5b6
Kim byungsung <lifee456@naver.com>
2022-01-10 18:46:16 +0900

    [2022.01.10]#수정부분

--- searchPage/search.js
+++ searchPage/search.js
@@ -1,7 +1,7 @@
 let URL = 'http://localhost:5000/searchKeyword';
 
 
-// 웹
+// 검색 조회
 const getQueryInURL = queryValue => {
 
     const reqObject = {
@@ -19,15 +19,15 @@ const getQueryInURL = queryValue => {
 
         })
     // 비디오 response
-    fetch(`https://dapi.kakao.com/v2/search/vclip?query=${queryValue}`, reqObject)
-        .then(res => res.json())
-        .then(result => {
-            // console.log(result.documents);
-            nav(result.documents);
-        })
+    // fetch(`https://dapi.kakao.com/v2/search/vclip?query=${queryValue}`, reqObject)
+    //     .then(res => res.json())
+    //     .then(result => {
+    //         // console.log(result);
+    //         nav(result.documents);
+    //     })
 }
 
-// 비디오
+// 비디오 조회
 const navURL = queryValue => {
     const reqObject = {
         method: "GET",
@@ -39,13 +39,67 @@ const navURL = queryValue => {
     fetch(`https://dapi.kakao.com/v2/search/vclip?query=${queryValue}`, reqObject)
         .then(res => res.json())
         .then(result => {
-            console.log(result.status);
-            // nav(result.documents);
+            console.log(result);
+            nav(result.documents);
         })
 
 }
 
-// 서버에서 통합검색 데이터 받아 렌더링
+// 이미지 조회
+const imgURL = queryValue => {
+    const reqObject = {
+        method: "GET",
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `KakaoAK 71f78f513d7f543d37d983c84b74d34f`
+        }
+    }
+    fetch(`https://dapi.kakao.com/v2/search/image?query=${queryValue}`, reqObject)
+        .then(res => res.json())
+        .then(result => {
+            // console.log(result);
+            img(result.documents);
+        })
+
+}
+
+// 블로그 조회
+const blogURL = queryValue => {
+    const reqObject = {
+        method: "GET",
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `KakaoAK 71f78f513d7f543d37d983c84b74d34f`
+        }
+    }
+    fetch(`https://dapi.kakao.com/v2/search/blog?query=${queryValue}`, reqObject)
+        .then(res => res.json())
+        .then(result => {
+            // console.log(result);
+            blog(result.documents);
+        })
+
+}
+
+// 카페 조회
+const cafeURL = queryValue => {
+    const reqObject = {
+        method: "GET",
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `KakaoAK 71f78f513d7f543d37d983c84b74d34f`
+        }
+    }
+    fetch(`https://dapi.kakao.com/v2/search/cafe?query=${queryValue}`, reqObject)
+        .then(res => res.json())
+        .then(result => {
+            // console.log(result);
+            cafe(result.documents);
+        })
+
+}
+
+// 통합검색 만드는중
 function get(res) {
     let tag = '';
     for (let i of res) {
@@ -59,12 +113,11 @@ function get(res) {
 
         tag += `
         <div class="serching">
-            <a href="#">
+            <a href="${url}">
                 <div class="title">${title}</div>
                 <div class="contents">${contents}</div>
-                <div class="url">도메인주소:${url}</div>
-                <div class="datetime">작성일:${datetime}</div>
             </a>
+            <div class="datetime">작성일:${datetime}</div>
         </div>
         `;
 
@@ -74,7 +127,7 @@ function get(res) {
 
 }
 
-// nav 만드는중
+// nav비디오클릭 만드는중
 function nav(res) {
 
     let tag = '';
@@ -91,14 +144,12 @@ function nav(res) {
 
         tag += `
         <div class="serching">
-            <a href="#">
+            <a href="${url}">
                 <div class="title">${title}</div>
-                <div class="url">도메인주소:${url}</div>
-                <div class="thumbnail">미리보기:${thumbnail}</div>
-                <div class="play_time">재생시간:${play_time}</div>
-                <div class="datetime">작성일:${datetime}</div>
-                <div class="author">업로더:${author}</div>
+                <img src="${thumbnail}" alt="미리보기">
             </a>
+            <div class="datetime">작성일:${datetime}</div>
+            <div class="author">업로더:${author}</div>
         </div>
         `;
 
@@ -107,6 +158,106 @@ function nav(res) {
     }
 
 }
+
+// 이미지 클릭 만드는중
+function img(res) {
+
+    
+    let tag = '';
+    for (let i of res) {
+
+        const {
+            collection,
+            datetime,
+            display_sitename,
+            doc_url,
+            thumbnail_url
+        } = i;
+
+        tag += `
+        <div class="searching">
+            <a href="${doc_url}">
+                <div class="collection">컬렉션:${collection}</div>
+                <img class="thumbnail_url" src="${thumbnail_url}">
+            </a>
+            <div class="datetime">작성일:${datetime}</div>
+            <div class="display_sitename">출처:${display_sitename}</div>
+        </div>
+        `;
+
+        const $ul = document.querySelector('#searchUl');
+        $ul.innerHTML = tag;
+    }
+
+}
+
+// 블로그 클릭만드는중
+function blog(res) {
+
+    let tag = '';
+    for (let i of res) {
+
+        const {
+            blogname,
+            contents,
+            datetime,
+            title,
+            url,
+            thumbnail
+        } = i;
+
+        tag += `
+        <div class="serching">
+            <a href="${url}">
+                <div class="title">${title}</div>
+                <div class="contents">${contents}</div>
+                <img src="${thumbnail}" alt="미리보기">
+                <div class="blogname">블로그명:${blogname}</div>
+            </a>
+            <div class="datetime">작성일:${datetime}</div>
+                
+        </div>
+        `;
+
+        const $ul = document.querySelector('#searchUl');
+        $ul.innerHTML = tag;
+    }
+
+}
+
+// 카페 클릭만드는중
+function cafe(res) {
+
+    let tag = '';
+    for (let i of res) {
+
+        const {
+            cafename,
+            contents,
+            datetime,
+            url,
+            title,
+            thumbnail
+        } = i;
+
+        tag += `
+        <div class="serching">
+            <a href="${url}">
+                <div class="cafename">카페명:${cafename}</div>
+                <div class="title">${title}</div>
+                <div class="contents">${contents}</div>
+                <img src="${thumbnail}" alt="미리보기">
+            </a>
+            <div class="datetime">작성일:${datetime}</div>
+        </div>
+        `;
+
+        const $ul = document.querySelector('#searchUl');
+        $ul.innerHTML = tag;
+    }
+
+}
+
 //===================================== 메인 실행부 ======================================
 (() => {
 
@@ -114,10 +265,42 @@ function nav(res) {
     console.log('쿼리:', usp.get('query'));
     getQueryInURL(usp.get('query'));
 
+    // 통합검색 
     document.querySelector('.naviUl').addEventListener('click', e => {
-        if (!e.target.matches('.naviUl .video a')) return;
+        if (!e.target.matches('.naviUl .search')) return;
         // console.log(e.target);
-        // nav(usp.nav('query'));
+        getQueryInURL(usp.get('query'));
+
+    })
+
+    // 비디오 
+    document.querySelector('.naviUl').addEventListener('click', e => {
+        if (!e.target.matches('.naviUl .video')) return;
+        // console.log(e.target);
+        navURL(usp.get('query'));
+
+    })
+    // 이미지
+    document.querySelector('.naviUl').addEventListener('click', e => {
+        if (!e.target.matches('.naviUl .img')) return;
+        // console.log(e.target);
+        imgURL(usp.get('query'));
+
+    })
+
+    // 블로그
+    document.querySelector('.naviUl').addEventListener('click', e => {
+        if (!e.target.matches('.naviUl .blog')) return;
+        // console.log(e.target);
+        blogURL(usp.get('query'));
+
+    })
+
+    // 카페
+    document.querySelector('.naviUl').addEventListener('click', e => {
+        if (!e.target.matches('.naviUl .cafe')) return;
+        // console.log(e.target);
+        cafeURL(usp.get('query'));
 
     })
 })();
