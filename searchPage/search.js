28d0bc631577bf9533a7cf342ef7fe6ca0575de8
kimbs95 <lifee456@naver.com>
2022-01-10 14:29:37 +0900

    [2022.01.10]#동영상부분

--- searchPage/search.js
+++ searchPage/search.js
@@ -14,10 +14,17 @@ const getQueryInURL = queryValue => {
     fetch(`https://dapi.kakao.com/v2/search/web?query=${queryValue}`, reqObject)
         .then(res => res.json())
         .then(result => {
+            // console.log(result);
             get(result.documents);
 
         })
-
+    // 비디오 response
+    fetch(`https://dapi.kakao.com/v2/search/vclip?query=${queryValue}`, reqObject)
+        .then(res => res.json())
+        .then(result => {
+            // console.log(result.documents);
+            nav(result.documents);
+        })
 }
 
 // 비디오
@@ -32,13 +39,13 @@ const navURL = queryValue => {
     fetch(`https://dapi.kakao.com/v2/search/vclip?query=${queryValue}`, reqObject)
         .then(res => res.json())
         .then(result => {
-            console.log(result);
+            console.log(result.status);
             // nav(result.documents);
         })
 
 }
 
-// 서버에서 데이터 받아 렌더링
+// 서버에서 통합검색 데이터 받아 렌더링
 function get(res) {
     let tag = '';
     for (let i of res) {
@@ -70,6 +77,35 @@ function get(res) {
 // nav 만드는중
 function nav(res) {
 
+    let tag = '';
+    for (let i of res) {
+
+        const {
+            play_time,
+            datetime,
+            title,
+            url,
+            author,
+            thumbnail
+        } = i;
+
+        tag += `
+        <div class="serching">
+            <a href="#">
+                <div class="title">${title}</div>
+                <div class="url">도메인주소:${url}</div>
+                <div class="thumbnail">미리보기:${thumbnail}</div>
+                <div class="play_time">재생시간:${play_time}</div>
+                <div class="datetime">작성일:${datetime}</div>
+                <div class="author">업로더:${author}</div>
+            </a>
+        </div>
+        `;
+
+        const $ul = document.querySelector('#searchUl');
+        $ul.innerHTML = tag;
+    }
+
 }
 //===================================== 메인 실행부 ======================================
 (() => {
@@ -79,8 +115,9 @@ function nav(res) {
     getQueryInURL(usp.get('query'));
 
     document.querySelector('.naviUl').addEventListener('click', e => {
-        if (!e.target.matches('.naviUl .video')) return;
-        navURL(queryValue);
+        if (!e.target.matches('.naviUl .video a')) return;
+        // console.log(e.target);
+        // nav(usp.nav('query'));
 
     })
 })();
